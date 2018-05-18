548236287766c18d4a47cc3ff003fecda9266717
Alex Bliss <alexander.blss@gmail.com>
2018-05-17 23:56:37 -0500

    fixed register route

--- server/routes/register.router.js
+++ server/routes/register.router.js
@@ -10,39 +10,74 @@ router.get('/', function(req, res, next) {
   res.sendFile(path.resolve(__dirname, '../public/views/templates/register.html'));
 });
 
-// Handles POST request with new user data
-router.post('/', function(req, res, next) {
+// // Handles POST request with new user data
+// router.post('/', function(req, res, next) {
 
-  var saveUser = {
-    username: req.body.username,
-    password: encryptLib.encryptPassword(req.body.password),
-    email: req.body.email,
-    real_name: req.body.real_name,
-    bio: req.body.bio
+//   var saveUser = {
+//     username: req.body.username,
+//     password: encryptLib.encryptPassword(req.body.password),
+//     email: req.body.email,
+//     real_name: req.body.real_name,
+//     bio: req.body.bio
       
-  };
-  console.log('new user:', saveUser);
+//   };
+//   console.log('new user:', saveUser);
 
-  pool.connect(function(err, client, done) {
-    if(err) {
-      console.log("Error connecting: ", err);
-      res.sendStatus(500);
-    }
-    client.query("INSERT INTO users (username, password, name, email) VALUES ($1, $2, $3, $4);",
-      [saveUser.username, saveUser.password, saveUser.real_name, saveUser.email],
+//   pool.connect(function(err, client, done) {
+//     if(err) {
+//       console.log("Error connecting: ", err);
+//       res.sendStatus(500);
+//     }
+//     client.query("INSERT INTO users (username, password, name, email) VALUES ($1, $2, $3, $4);",
+//       [saveUser.username, saveUser.password, saveUser.real_name, saveUser.email],
+//         function (err, result) {
+//           client.end();
+
+//           if(err) {
+//             console.log("Error inserting data: ", err);
+//             res.sendStatus(500);
+//           } else {
+//             res.sendStatus(201);
+//           }
+//         });
+//   });
+
+// });
+
+
+router.post('/', function (req, res, next) {
+  if (req.isAuthenticated() && req.user.is_super_admin) {
+    var saveUser = {
+      username: req.body.username,
+      password: encryptLib.encryptPassword(req.body.password),
+      is_admin: true,
+      is_super_admin: req.body.is_super_admin,
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+    };
+    console.log('new user:', saveUser);
+    pool.connect(function (err, client, done) {
+      if (err) {
+        console.log("Error connecting: ", err);
+        res.sendStatus(500);
+      }
+      client.query("INSERT INTO users (username, password, is_admin, is_super_admin, first_name, last_name, email) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING user_id",
+        [saveUser.username, saveUser.password, saveUser.is_admin, saveUser.is_super_admin, saveUser.first_name, saveUser.last_name, saveUser.email],
         function (err, result) {
           client.end();
-
-          if(err) {
+          if (err) {
             console.log("Error inserting data: ", err);
             res.sendStatus(500);
           } else {
             res.sendStatus(201);
           }
         });
-  });
-
+    });
+  } else {
+    console.log('not logged in');
+    res.send(false);
+  }
 });
-
 
 module.exports = router;
